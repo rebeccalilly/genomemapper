4fa9bc69f298ca04e026e9e85f6b592ec5894dd1
AnnieL2 <annieli3011@gmail.com>
2022-04-16 20:10:48 -0400

    Add comments

--- dcsfinal.py
+++ dcsfinal.py
@@ -19,18 +19,19 @@ def runBlast(sequence_file: str) -> list:
     Returns:
         accession numbers for 50 top hits, as list
     '''
+    # if the xml file which contains the blast outputs does not exist, use qblast to invoke the NCBI BLAST server over the internet and save the outputs on the local machine as an xml file:
     if not (os.path.exists("results.xml")):
         sequence_data = open(sequence_file).read()
         bar = Bar("Processing sequence data... ")
-        result_handle = NCBIWWW.qblast("blastn", "nt", sequence_data)
+        result_handle = NCBIWWW.qblast("blastn", "nt", sequence_data) #blastn = program, "nt" = database to search against, in this case, "nt" stands for the nucleotide databse
         bar.finish()
 
-        with open("results.xml","w") as save_file:
+        with open("results.xml","w") as save_file: # write the blastn output to a xml file
             blast_results = result_handle.read()
             save_file.write(blast_results)
 
     results = open("results.xml","r")
-    records = list(NCBIXML.parse(results)) #parse results (xml file) into a list
+    records = list(NCBIXML.parse(results)) # parse results (xml file) into a list
     one_query = records[0] #from a list to a record class that holds all BLAST output
 
     accession_numbers = [] #create empty list to store the accession_numbers of each organism
@@ -53,35 +54,40 @@ def getLocationsAuthors(accession_numbers: list) -> list:
         location of institution and first author associated with 50 top hits
         from BlastN output
     '''
-    #parsing GenBank data
+
     Entrez.email = input("Enter your email address (so that NCBI can contact you if there's a problem): ")
     bar = Bar("Processing accession numbers in GenBank:", max = len(accession_numbers))
 
+    # create two empty lists that store the location and author informations
     location_list = []
     author_list = []
+
+    # parse GenBank data for every matching sequence (accession number)
     for i in accession_numbers:
         handle = Entrez.efetch(db = 'nucleotide', rettype = 'gb', retmode = 'text', id = i)
         record = handle.read()
-        info = record.split("\n")
-        location_line = [i for i in info if i.startswith('  JOURNAL   Submitted') or i.startswith('FEATURES') or i.startswith('COMMENT')]
+        info = record.split("\n") # split the text file by new line
+        location_line = [i for i in info if i.startswith('  JOURNAL   Submitted') or i.startswith('FEATURES') or i.startswith('COMMENT')] # extract lines that starts with '  JOURNAL   Submitted' which contains the location information, and lines that starts with 'FEATURES' or 'COMMENT' which are the lines right after the location information
         location_line_indices = []
+
         for j in location_line:
-            index_number = info.index(j)
+            index_number = info.index(j) #extract the index of the locations line
             location_line_indices.append(index_number)
 
-        index_1 = location_line_indices[0]
-        index_2 = location_line_indices[-1]
+        index_1 = location_line_indices[0] # first location line
+        index_2 = location_line_indices[-1] # last location line
 
         location_info = info[index_1:index_2]
         first_line = location_info[0]
-        first_line_split = first_line.split(") ")
+        first_line_split = first_line.split(") ") # all location lines begin with 'JOURNAL Submitted (date)', so split the line accordingly and only store the relevant information
         cleaned_first_line = first_line_split[1]
         location = cleaned_first_line
 
-        for h in location_info[1:]:
-            cleaned_line = h.lstrip(" ")
-            location += cleaned_line
+        for h in location_info[1:]: # for the rest of the location lines
+            cleaned_line = h.lstrip(" ") # delete the extra spaces in front of the first letter
+            location += cleaned_line # append the info to the first line
 
+        # filter out all possible irrelevant information:
         if "COMMENT" in location:
             updated_location = location.split("COMMENT")
             location = updated_location[0]
@@ -99,8 +105,8 @@ def getLocationsAuthors(accession_numbers: list) -> list:
            author_line_indices.append(index_number_)
 
            index_reference = author_line_indices[0]
-           index_author = index_reference + 1
-           index_title = author_line_indices[-1]
+           index_author = index_reference + 1 # author information starts on the next line
+           index_title = author_line_indices[-1] # title line is the line right after the author info
            author_info = info[index_author:index_title]
 
         try:
@@ -116,9 +122,9 @@ def getLocationsAuthors(accession_numbers: list) -> list:
             author_list.append(author)
 
 
-        except IndexError as error:
+        except IndexError as error: # the line reference 2 may not exist if there is only one reference
             author_line_indices = []
-            author_line = [g for g in info if g.startswith('  AUTHORS') or g.startswith('  TITLE     Direct Submission')]
+            author_line = [g for g in info if g.startswith('  AUTHORS') or g.startswith('  TITLE     Direct Submission')] # search for line that starts with 'AUTHORS' instead of 'Reference 2'
             for l in author_line:
                 index_number_ = info.index(l)
                 author_line_indices.append(index_number_)
@@ -146,13 +152,13 @@ def getAddress(locations: list):
     '''
     '''
     address_list = []
-    regex_ = "([\sa-zA-Z0-9-]+,[\sa-zA-Z]+[\s0-9a-zA-Z-]+,[a-zA-Z\s]+)$"
+    regex_ = "([\sa-zA-Z0-9-]+,[\sa-zA-Z]+[\s0-9a-zA-Z-]+,[a-zA-Z\s]+)$" # regular expression to get the specific address that exludes the institution names and in some cases the street name as well
     for i in locations:
         result = re.findall(regex_, i)
         final_result = " ".join(result)
         cleaned_result = final_result.strip()
         split_address = cleaned_result.split(",")
-        first, second = split_address[0], split_address[2]
+        first, second = split_address[0], split_address[2] # only need the street/city and the country. The zip code info which belongs to split_address[1] is eliminated as Nominatim is unable to process zip code.
         address = first, second
         valid_address = ', '.join(address)
         address_list.append(valid_address)
@@ -177,6 +183,12 @@ def getLatLong(address: str) -> list:
 
 def getLatLongLists(address_list: list) -> list:
     '''
+    Input address to the getLatLong function and returns a latitude list and a longitude list
+
+    Args:
+        address_list: a list of strings that correspond to specific address
+    Returns:
+        a list that contains two list: a latitude list and a longitude list
     '''
     lat_list = []
     long_list = []
@@ -185,11 +197,13 @@ def getLatLongLists(address_list: list) -> list:
         try:
             lat_long = getLatLong(i)
         except AttributeError as error:
+            print(" ")
             print(f"Unable to fetch the following address using Nominatim: {i}")
-            new_address = input("Enter valid address in 'city, country' format: ")
+            new_address = input("Enter valid address in 'city, country' format: ") # in case a foreign street name was the problem that Nominatim cannot process the address info
             try:
                 lat_long = getLatLong(new_address)
             except AttributeError as error:
+                print(" ")
                 print("Invalid Address entered")
                 lat_long = ["Invalid Address", "Invalid Address"]
         lat_list.append(lat_long[0])
